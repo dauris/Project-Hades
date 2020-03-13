20b2c5ec27d80d722eb6052c7eb99243b9c357f5
mechanicninja <dauris.little@gmail.com>
2020-03-13 15:51:57 -0400

    Project Athena Console Application w/Core created

---
+++ Project Mount Olympus/Athena/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Athena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
